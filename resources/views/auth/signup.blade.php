eac1f198af0382c8b1357d04b2318a12f5b8c24c
Red Ren <jpren2000@gmail.com>
2023-11-15 06:17:30 +0800

    bonak

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="{{asset('js/jquery.js')}}"></script>
-  @vite('resources/css/app.css')
+  <script src="https://cdn.tailwindcss.com"></script>
   {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
   <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.js" defer></script>
 
