9d9a02028069ee5fd8bd4447fc7e2d0b05f31cb6
Red Ren <jpren2000@gmail.com>
2023-11-28 21:56:18 +0800

    s

--- app/Imports/Imports_data.php
+++ app/Imports/Imports_data.php
@@ -5,7 +5,6 @@ namespace App\Imports;
 use App\Models\Insurance;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class Imports_data implements ToModel, WithStartRow
 {
@@ -17,30 +16,30 @@ class Imports_data implements ToModel, WithStartRow
     public function model(array $row)
     {
         $id = mt_rand(111111111, 999999999);
-        // Convert Excel date serial number to a Unix timestamp
-        $birthday = !empty($row[3]) ? Date::excelToTimestamp($row[3]) : null;
-        $startAt = !empty($row[6]) ? Date::excelToTimestamp($row[6]) : null;
-        $endAt = !empty($row[7]) ? Date::excelToTimestamp($row[7]) : null;
+    // Convert Excel date serial number to a Unix timestamp
+    $birthday = !empty($row[2]) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[2]) : null;
+    $startAt = !empty($row[7]) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[7]) : null;
+    $endAt = !empty($row[8]) ? \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[8]) : null;
 
-        // Convert Unix timestamp to 'YYYY-MM-DD' format
-        $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
-        $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
-        $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
+    // Convert Unix timestamp to 'YYYY-MM-DD' format
+    $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
+    $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
+    $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
 
         return new Insurance([
             'id' => $id,
-            'fname' => $row[1],
-            'lname' => $row[2],
+            'fname' => $row[0],
+            'lname' => $row[1],
             'birthday' => $formattedBirthday,
-            'municipality' => $row[4],
-            'type_of_payment' => $row[5],
+            'municipality' => $row[3],
+            'level' => $row[4],
+            'status' => $row[5],
+            'type_of_payment' => $row[6],
             'start_at' => $formattedStartAt,
-            'end_at' => $formattedEndAt,
-            'mem_id' => $row[8],
-            'OR#' => $row[9],
-            'level' => 'CLASSIC',
-            'status' => "ACTIVATED",
-
+            'end_at' => $formattedEndAt,    
+            'mem_id' => $row[9],
+            'OR#' => $row[10],
+            // Map other columns as needed
         ]);
     }
 }
