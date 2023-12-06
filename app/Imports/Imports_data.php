6af7373d069ebdc099acb19c3eb5388b1bdd4e94
Red Ren <jpren2000@gmail.com>
2023-12-06 17:48:12 +0800

    try

--- app/Imports/Imports_data.php
+++ app/Imports/Imports_data.php
@@ -5,7 +5,9 @@ namespace App\Imports;
 use App\Models\insurance;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
+use DateTime; // Import DateTime class for working with dates
+use PhpOffice\PhpSpreadsheet\Shared\Date; // Import Date class for Excel date conversion if not already imported
+
 
 class Imports_data implements ToModel, WithStartRow
 {
@@ -18,31 +20,55 @@ class Imports_data implements ToModel, WithStartRow
 
     public function model(array $row)
     {
-        $id = mt_rand(111111111, 999999999);
-
-        $birthday = !empty($row[3]) && is_numeric($row[3]) ? Date::excelToTimestamp($row[3]) : null;
-        $startAt = !empty($row[6]) && is_numeric($row[6]) ? Date::excelToTimestamp($row[6]) : null;
-        $endAt = !empty($row[7]) && is_numeric($row[7]) ? Date::excelToTimestamp($row[7]) : null;
-
-        $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
-        $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
-        $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
+        // Check if any data exists in the row before attempting to process
+        if (!empty($row[1])) {
+            $id = mt_rand(111111111, 999999999);
 
 
-        return new insurance([
-            'id' => $id,
-            'mem_id' => $row[9],
-            'level' => $row[8], 
-            'fname' => $row[1],
-            'lname' => $row[2],
-            'birthday' => $formattedBirthday,
-            'municipality' => $row[4],
-            'status' => "ACTIVATED",
-            'type_of_payment' => $row[5],
-            'start_at' => $formattedStartAt,
-            'end_at' => $formattedEndAt,
-            'OR#' => $row[10],
-           // Retrieve the last added sheet name for the current row
-        ]);
+            $dateString = $row[7]; // Replace with the actual cell content or formula result
+
+            // Convert the formatted date string to a timestamp using DateTime
+            $timestamp = null;
+            if (!empty($dateString)) {
+                $dateTime = DateTime::createFromFormat('F j, Y', $dateString); // Adjust the format according to your date format
+                if ($dateTime) {
+                    $timestamp = $dateTime->getTimestamp();
+                } else {
+                    // If DateTime conversion fails, try using Date::excelToTimestamp()
+                    $timestamp = Date::excelToTimestamp($dateString);
+                }
+            }
+            
+         
+            
+            
+
+            $birthday = !empty($row[3]) && is_numeric($row[3]) ? Date::excelToTimestamp($row[3]) : null;
+            $startAt = !empty($row[6]) && is_numeric($row[6]) ? Date::excelToTimestamp($row[6]) : null;
+            // $endAt = !empty($row[7]) && is_numeric($row[7]) ? Date::excelToTimestamp($row[7]) : null;
+            $endAt = $timestamp !== null ? $timestamp : null;
+
+            $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
+            $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
+            $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
+
+            return new insurance([
+                'id' => $id,
+                'mem_id' => $row[9],
+                'level' => $row[8],
+                'fname' => $row[1],
+                'lname' => $row[2],
+                'birthday' => $formattedBirthday,
+                'municipality' => $row[4],
+                'status' => "ACTIVATED",
+                'type_of_payment' => $row[5],
+                'start_at' => $formattedStartAt,
+                'end_at' => $formattedEndAt,
+                'OR#' => $row[10],
+            ]);
+        }
+
+        // Return null if the row is empty or missing crucial data
+        return null;
     }
 }
