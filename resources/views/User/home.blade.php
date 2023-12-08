f1afa684503ac76b5666ad8cea159175e9e4d887
Red Ren <jpren2000@gmail.com>
2023-12-08 13:24:00 +0800

    Update home.blade.php

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<div class="w-full overflow-y-auto ">
+<div class="w-full">
 
     <!-- <video autoplay loop muted playsinline preload="auto" class="w-full h-full object-cover absolute inset-0">
       <source src="static/user/animated.webm" type="video/webm">
