5257ad4c4aaec2ff1e6644c06b755642e1ad1e6e
Red Ren <jpren2000@gmail.com>
2023-12-08 16:30:59 +0800

    Update Imports_data.php

--- app/Imports/Imports_data.php
+++ app/Imports/Imports_data.php
@@ -10,51 +10,43 @@ use PhpOffice\PhpSpreadsheet\Shared\Date;
 class Imports_data implements ToModel, WithStartRow
 {
 
-
-
-public function model(array $row)
-{
-    // Get the value from the first cell of the row (assuming it's in the first column)
-    $category = $row[0] ?? null;
-
-    // Check if any data exists in the row before attempting to process
-    if (!empty($row[1])) {
-        $id = mt_rand(111111111, 999999999);
-
-        $birthday = !empty($row[3]) && is_numeric($row[3]) ? Date::excelToTimestamp($row[3]) : null;
-        $startAt = !empty($row[6]) && is_numeric($row[6]) ? Date::excelToTimestamp($row[6]) : null;
-        $endAt = !empty($row[7]) && is_numeric($row[7]) ? Date::excelToTimestamp($row[7]) : null;
-
-        $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
-        $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
-        $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
-
-        return new insurance([
-            'id' => $id,
-            'mem_id' => $row[8],
-            'level' => $category,
-            'fname' => $row[1],
-            'lname' => $row[2],
-            'birthday' => $formattedBirthday,
-            'municipality' => $row[4],
-            'status' => "ACTIVATED",
-            'type_of_payment' => $row[5],
-            'start_at' => $formattedStartAt,
-            'end_at' => $formattedEndAt,
-            'OR#' => $row[9],
-        ]);
-
-        
+    public function startRow(): int
+    {
+        return 2; // Start importing from the 2nd row (row index 1)
     }
 
 
-    // Return null if the row is empty or missing crucial data
-    return null;
-    
-}
-public function startRow(): int
-{
-    return 3; // Start importing from the 2nd row (row index 1)
-}
+    public function model(array $row)
+    {
+        // Check if any data exists in the row before attempting to process
+        if (!empty($row[1])) {
+            $id = mt_rand(111111111, 999999999);
 
+            $birthday = !empty($row[3]) && is_numeric($row[3]) ? Date::excelToTimestamp($row[3]) : null;
+            $startAt = !empty($row[6]) && is_numeric($row[6]) ? Date::excelToTimestamp($row[6]) : null;
+            $endAt = !empty($row[7]) && is_numeric($row[7]) ? Date::excelToTimestamp($row[7]) : null;
+
+            $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
+            $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
+            $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
+
+            return new insurance([
+                'id' => $id,
+                'mem_id' => $row[9],
+                'level' => $row[8],
+                'fname' => $row[1],
+                'lname' => $row[2],
+                'birthday' => $formattedBirthday,
+                'municipality' => $row[4],
+                'status' => "ACTIVATED",
+                'type_of_payment' => $row[5],
+                'start_at' => $formattedStartAt,
+                'end_at' => $formattedEndAt,
+                'OR#' => $row[10],
+            ]);
+        }
+
+        // Return null if the row is empty or missing crucial data
+        return null;
+    }
 }
