0adcd2beb5989480b9683d906f86b4a9a4a68769
Red Ren <jpren2000@gmail.com>
2023-12-08 15:59:48 +0800

    Update Imports_data.php

--- app/Imports/Imports_data.php
+++ app/Imports/Imports_data.php
@@ -2,45 +2,42 @@
 
 namespace App\Imports;
 
-use App\Models\insurance;
+use App\Models\Insurance;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class Imports_data implements ToModel, WithStartRow
 {
+    protected $level; // Variable to store the CLASSIC value
 
     public function startRow(): int
     {
-        return 3; // Start importing from the 2nd row (row index 1)
+        return 3; // Start importing from the 3rd row (row index 2)
     }
-
-    protected $category;
 
     public function model(array $row)
     {
-        // Set the 'level' value from the first row's first column
-        if (empty($this->category) && !empty($row[0])) {
-            $this->category = $row[0];
-            return null; // Skip the first row as it's a header
+        // Get the CLASSIC value from the first row, first column
+        if (empty($this->level) && isset($row[0])) {
+            $this->level = $row[0];
+            return null; // Skip processing the header row
         }
 
         // Check if any data exists in the row before attempting to process
         if (!empty($row[1])) {
-            $id = mt_rand(111111111, 999999999);
-
-            $birthday = !empty($row[3]) && is_numeric($row[3]) ? Date::excelToTimestamp($row[3]) : null;
-            $startAt = !empty($row[6]) && is_numeric($row[6]) ? Date::excelToTimestamp($row[6]) : null;
-            $endAt = !empty($row[7]) && is_numeric($row[7]) ? Date::excelToTimestamp($row[7]) : null;
+            $birthday = !empty($row[3]) ? Date::excelToTimestamp($row[3]) : null;
+            $startAt = !empty($row[6]) ? Date::excelToTimestamp($row[6]) : null;
+            $endAt = !empty($row[7]) ? Date::excelToTimestamp($row[7]) : null;
 
             $formattedBirthday = $birthday !== null ? date('Y-m-d', $birthday) : null;
             $formattedStartAt = $startAt !== null ? date('Y-m-d', $startAt) : null;
             $formattedEndAt = $endAt !== null ? date('Y-m-d', $endAt) : null;
 
-            return new insurance([
-                'id' => $id,
+            return new Insurance([
                 'mem_id' => $row[8],
-                'level' => $this->category,
+                'level' => $this->level, // Set the CLASSIC value obtained from the first row
                 'fname' => $row[1],
                 'lname' => $row[2],
                 'birthday' => $formattedBirthday,
