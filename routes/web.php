7f007b6bbc79a81dbb06b666b6f0bfe20c699609
Red Ren <jpren2000@gmail.com>
2023-12-09 14:42:43 +0800

    s

--- routes/web.php
+++ routes/web.php
@@ -195,9 +195,9 @@ Route::post('share-feedback',[Feedback::class,'Create_Feedback']);
 
 
 // IMPORT
-Route::post('/import_membership_excel_file', [Imports::class, 'import_membership_excel_file'])->name('import');
+Route::post('/import_membership_excel_file', [Imports::class, 'import_membership_excel_file'])->name('import_membership_excel_file');
 
-Route::post('/import_volunteer_excel_file', [Imports::class, 'import_volunteers_excel_file'])->name('import');
+Route::post('/import_volunteers_excel_file', [Imports::class, 'import_volunteers_excel_file'])->name('import_volunteers_excel_file');
 
 
 
