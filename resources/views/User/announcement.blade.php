127b36f4575724c8e0f343a809dbf49247264635
Red Ren <jpren2000@gmail.com>
2023-12-10 22:48:09 +0800

    Update announcement.blade.php

--- resources/views/User/announcement.blade.php
+++ resources/views/User/announcement.blade.php
@@ -8,11 +8,11 @@
   <div class="container mx-auto">
     <div class="max-w-3xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
       <div class="p-6">
-        <h2 class="text-4xl font-semibold mb-2">{{ $announcements->title }}</h2>
+        <h2 class="text-4xl font-semibold mb-2">{{ $announcement->title }}</h2>
         <p class="text-sm text-gray-500 mb-4">
-          Posted on {{ \Carbon\Carbon::parse($announcements->created_at)->format('F d, Y h:i A') }} by PRC ORMIN CHAPTER
+          Posted on {{ \Carbon\Carbon::parse($announcement->created_at)->format('F d, Y h:i A') }} by PRC ORMIN CHAPTER
         </p>
-        <p class="text-lg text-gray-700 leading-relaxed">{{ $announcements->announcement }}</p>
+        <p class="text-lg text-gray-700 leading-relaxed">{{ $announcement->announcement }}</p>
       </div>
       <div class="bg-gray-100 px-6 py-4 flex items-center justify-between">
         <button onclick="goBack()" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
