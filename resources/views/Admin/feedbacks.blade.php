4e9563dfdf90cac4038550f0ec155885b9cb8565
Red Ren <jpren2000@gmail.com>
2023-12-10 16:48:05 +0800

    Update feedbacks.blade.php

--- resources/views/Admin/feedbacks.blade.php
+++ resources/views/Admin/feedbacks.blade.php
@@ -14,7 +14,7 @@
 </div>
 
 <div id="show-feedback-details-modal" class="fixed hidden inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50 overflow-y-auto">
-  <div class="modal-container bg-white sm:w-1/4 lg:w-1/4 rounded-lg shadow-lg mx-5">
+  <div class="modal-container bg-white sm:w-1/4 lg:w-3/5 xl:3/5 rounded-lg shadow-lg mx-5">
     <div class="px-4 py-3">
       <p class="text-2xl text-center font-semibold text-green-500">Feedback's Overview</p>
     </div>
@@ -91,15 +91,15 @@
         var details = "<div>";
 
         details += "<p> <span class='text-sm'>Feedback</span> </p>";
-        details += "<p class='text-xs'>" + response.details.created_at + "</p>";
-        details += "<p class='border-y-2 border-gray-200 p-2'>" + response.details.message + "</p>";
+        details += "<p class='text-xs'>" + response.created_at + "</p>";
+        details += "<p class='border-y-2 border-gray-200 p-2'>" + response.message + "</p>";
 
         details += "<p> <span class='text-sm'>Sender</span> </p>";
-        details += "<p>" + response.details.identity + "</p>";
-        details += "<p>" + response.details.u_id + "</p>";
+        details += "<p>" + response.identity + "</p>";
+        details += "<p>" + response.u_id + "</p>";
 
         details += "<button type='button' id='show-feedback-modal-btn' class=' px-2 py-1 rounded-md bg-blue-500 text-white font-semibold'>Close</button>";
-        details += "<button type='button' id='delete-btn' data-id=" + response.details.id + " class='px-2 py-1 rounded-md bg-red-500 text-white font-semibold'>Delete</button>";
+        details += "<button type='button' id='delete-btn' data-id=" + response.id + " class='px-2 py-1 rounded-md bg-red-500 text-white font-semibold'>Delete</button>";
 
         details += "</div>";
         $('#feedback-details').empty().append(details);
