3be83d964d2b457af7d2793f3b437ead14a2855a
Red Ren <jpren2000@gmail.com>
2023-12-11 19:59:21 +0800

    Update announcement.blade.php

--- resources/views/User/announcement.blade.php
+++ resources/views/User/announcement.blade.php
@@ -6,20 +6,18 @@
 
 <div class="h-screen w-full overflow-y-auto ">
 
+    <div class="m-4 bg-w p-4hite rounded-lg shadow-md overflow-hidden">
 <h2 class="text-2xl text-gray-400 mb-2">PRC ORMIN | Announcement | {{ $announcement->id }}</h2>
 
-
-
-    <div class="max-w-3xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
       <div class="p-6">
         <h2 class="text-4xl font-semibold mb-2">{{ $announcement->title }}</h2>
         <p class="text-sm text-gray-500 mb-4">
           Posted on {{ \Carbon\Carbon::parse($announcement->created_at)->format('F d, Y h:i A') }} by PRC ORMIN CHAPTER
         </p>
-       
-      </div>
-      <p class="text-lg text-gray-700 leading-relaxed">{{ $announcement->announcement }}</p>
+        <p class="text-lg text-gray-700 leading-relaxed">{{ $announcement->announcement }}</p>
 
+      </div>
+     
       <div class="bg-gray-100 px-6 py-4 flex items-center justify-between">
         <button onclick="goBack()" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
           Back
