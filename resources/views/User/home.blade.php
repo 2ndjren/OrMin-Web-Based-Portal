d510eabbbe539ff669a39f8ebcfe46e5ef75e093
Red Ren <jpren2000@gmail.com>
2023-12-11 21:07:45 +0800

    Update home.blade.php

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -158,26 +158,36 @@
   </div>
 
 
-  <div class="bg-center bg-no-repeat bg-cover ease-in-out transition-opacity">
-    <div class="p-4 md:p-28 bg-green-600 bg-opacity-75">
-      <div class="flex flex-col-reverse md:flex-row w-full">
-        <div class="w-full md:w-1/2 space-y-4 md:px-10 py-4 md:py-10">
-          <p class="text-2xl md:text-4xl font-bold text-white">Be a Red Cross Member</p>
-          <div class="space-y-2">
-            <p class="text-white font-semibold">Joining the Philippine Red Cross Membership Program gives individuals a sense of self-worth as you extend help to the most vulnerable Filipinos.</p>
-            <p class="text-white font-semibold">The Membership Fund Drive program grants you access to all services of the Philippine Red Cross during emergencies, sickness, and disasters.</p>
-          </div>
-          <p class="text-white font-semibold">GET THE NEW PLATINUM CARD FOR ONLY P1,200! THIS COMES WITH 2 NEW BENEFITS: FREE EMERGENCY AMBULANCE SERVICE AND 1 UNIT OF WHOLE BLOOD.</p>
-          <div class="py-5">
-            <a class="text-white bg-green-500 rounded-md shadow-md py-2 px-4 md:px-5" href="/membership">LEARN MORE...</a>
-          </div>
+
+  <div class="bg-gray-200 p-4 md:p-28 flex flex-col-reverse md:flex-row w-full">
+     
+      <div class="w-full md:w-2/3 space-y-4 md:space-y-6 px-4 md:px-10">
+        <h1 class="text-4xl md:text-4xl xl:text-6xl font-bold mb-2 md:mb-4 text-white text-center md:text-right" data-aos="fade-up">
+        Be a Red Cross Member
+        </h1>
+        <div class="text-white text-center md:text-right md:text-xl">
+        Joining the Philippine Red Cross Membership Program gives individuals a sense of self-worth as you extend help to the most vulnerable Filipinos.
+            The Membership Fund Drive program grants you access to all services of the Philippine Red Cross during emergencies, sickness, and disasters.
         </div>
-        <div class="w-full md:w-1/2 flex justify-center py-4 md:py-10">
-          <img class="border-4 border-white shadow-md bg-center bg-cover bg-no-repeat h-48 md:h-1/2" src="https://redcross.org.ph/wp-content/themes/yootheme/cache/376370625_239587185277034_3826957384081035787_n-f265f1e7.jpeg" alt="">
+        <div class="text-center md:text-right">
+          <ul class="mt-4">
+            <li>
+              <a href="donate#blood" class="bg-yellow-600 hover:bg-white text-black font-bold hover:text-red-600 py-2 px-4 border-4 hover:border-transparent rounded">
+                DONATE BLOOD!
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="w-full md:w-1/3 pb-4 md:pb-0">
+        <div class="py-4 transition-transform ease-in-out transform hover:scale-110">
+        <img class="border-4 border-white shadow-md bg-center bg-cover bg-no-repeat h-48 md:h-1/2" src="https://redcross.org.ph/wp-content/themes/yootheme/cache/376370625_239587185277034_3826957384081035787_n-f265f1e7.jpeg" alt="">
         </div>
       </div>
     </div>
-  </div>
+
+
+   
 
 
 
