7a989970f196d1eb6a5a1ec6a3266c4c5371ff8c
Red Ren <jpren2000@gmail.com>
2023-12-11 22:46:19 +0800

    Update layout.blade.php

--- resources/views/layout/admin/layout.blade.php
+++ resources/views/layout/admin/layout.blade.php
@@ -18,11 +18,7 @@
 </head>
 
 <body class="bg-gray-100 font-sans">
-
-
-    <div class="flex h-screen">
-
-    <header class=" justify-between items-center bg-gray-50 text-gray-900 p-6 border-b-2 border-gray-300">
+<header class="fixed justify-between items-center bg-gray-50 text-gray-900 p-6 border-b-2 border-gray-300">
                 <!-- Header content -->
                 <h1 class="font-bold text-blue-800">PRC MINDORO ORIENTAL CHAPTER</h1>
 
@@ -43,6 +39,8 @@
 
                 </div>
             </header>
+            
+    <div class="flex h-screen">
 
        <aside class="w-64 text-white hidden fixed flex-shrink-0 lg:flex flex-col items-center relative bg-cover bg-center overflow-y-auto" style="background-image: url('static/user/home/sidebar.jpg');">
 
