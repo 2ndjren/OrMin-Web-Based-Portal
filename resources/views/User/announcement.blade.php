ffa86a550730bd5f7ccf27c8266a8b39c92e6907
Red Ren <jpren2000@gmail.com>
2023-12-11 20:45:30 +0800

    Update announcement.blade.php

--- resources/views/User/announcement.blade.php
+++ resources/views/User/announcement.blade.php
@@ -1,44 +1,37 @@
 @extends('layout.user.layout')
+
 @section('announcement')
+  <title>PRC ORMIN | Announcement</title>
 
+  <div class="min-h-screen flex flex-col justify-between bg-gray-100">
+    <div class="m-6 bg-white rounded-lg shadow-md">
+      <h2 class="text-lg text-gray-500 my-2 px-6 py-4 border-b border-gray-200">
+        PRC ORMIN | Announcement | {{ $announcement->id }}
+      </h2>
 
-<title>PRC ORMIN|Announcement</title>
+      <div class="p-8">
+        <h2 class="text-4xl font-semibold mb-2">{{ $announcement->title }}</h2>
+        <p class="text-sm text-gray-500 mb-4">
+          Posted on {{ \Carbon\Carbon::parse($announcement->created_at)->format('F d, Y h:i A') }} by PRC ORMIN CHAPTER
+        </p>
+        <p class="text-lg text-gray-700 leading-relaxed text-justify">
+          {{ $announcement->announcement }}
+        </p>
+      </div>
 
-<div class="h-screen w-full overflow-y-auto ">
-
-  <div class="m-4 bg-gray-200 p-8 rounded-lg shadow-md overflow-hidden">
-    <h2 class="text-lg text-gray-500 my-2">PRC ORMIN | Announcement | {{ $announcement->id }}</h2>
-
-    <div class="p-6">
-      <h2 class="text-4xl font-semibold mb-2">{{ $announcement->title }}</h2>
-      <p class="text-sm text-gray-500 mb-4">
-        Posted on {{ \Carbon\Carbon::parse($announcement->created_at)->format('F d, Y h:i A') }} by PRC ORMIN CHAPTER
-      </p>
-      <p class="text-lg text-gray-700 leading-relaxed" style="text-align: justify;">{{ $announcement->announcement }}</p>
-
+      <div class="bg-gray-100 px-6 py-4 flex items-center justify-between">
+        <button onclick="goBack()" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
+          Back
+        </button>
+      </div>
     </div>
 
-    <div class="bg-gray-100 px-6 py-4 flex items-center justify-between">
-      <button onclick="goBack()" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
-        Back
-      </button>
-    </div>
-  </div>
-
-
-
-  <div class="">
     @include('layout.user.footer')
   </div>
 
   <script>
     function goBack() {
-      // Navigate back in the browser's history
       window.history.back();
     }
   </script>
-
-
-
-
-  @endsection
+@endsection
